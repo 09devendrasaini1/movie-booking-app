0593095dd4262594b0234e2713f43fcda9496f6e
09devendrasaini1 <09devendrasaini1@gmail.com>
2026-07-15 20:48:56 +0530

    Update MongoDB Atlas connection

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/bookmyshow');
-        console.log('Connected to Database');
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to Database");
     } catch (err) {
-        console.log('Database connection error:', err);
+        console.log("Database connection error:", err);
         process.exit(1);
     }
 };
